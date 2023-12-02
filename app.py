e5de8f9224003457b74e7331c753bd99b456ed2d
klongklonginthehouse <151914835+klongklonginthehouse@users.noreply.github.com>
2023-12-03 07:49:35 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -8,18 +8,8 @@ def home():
     st.write("Welcome to the home page!")
     st.write("Hi👋 Im Jave Gajelloma, a Computer Engineering student")
     
-    st.image("https://scontent.fdvo2-2.fna.fbcdn.net/v/t39.30808-6/407316958_873382151242463_1150520590098207901_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeH8vlH__h-msRZ3fs_VXIdSWzi4og05C7lbOLiiDTkLuVTKOqf_e19NNHuTnZ68nWgmXZh7K5snbOwYTb0R3lhv&_nc_ohc=B8yCt84wjXQAX-aM7CI&_nc_ht=scontent.fdvo2-2.fna&oh=00_AfAlsReEoS0Ry3LYsuFLQHatPpWCIcoq1ubeEEjHI1XX3g&oe=6570F18F", caption="Click the Play Button", use_column_width=True)
-    st.markdown(f"""
-        <style>
-            .image-with-border {{
-                border: 2px solid #FF0000; /* Red border */
-                border-radius: 5px; /* Optional: Adds rounded corners */
-                padding: 5px; /* Optional: Adds padding to the border */
-            }}
-        </style>
-    """, unsafe_allow_html=True)
-    st.audio("Music/never-gonna-give-you-up-made-with-Voicemod-technology.mp3", format="audio/mp3", start_time=0)
-
+    st.image("https://scontent.fdvo2-2.fna.fbcdn.net/v/t39.30808-6/407316958_873382151242463_1150520590098207901_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeH8vlH__h-msRZ3fs_VXIdSWzi4og05C7lbOLiiDTkLuVTKOqf_e19NNHuTnZ68nWgmXZh7K5snbOwYTb0R3lhv&_nc_ohc=B8yCt84wjXQAX-aM7CI&_nc_ht=scontent.fdvo2-2.fna&oh=00_AfAlsReEoS0Ry3LYsuFLQHatPpWCIcoq1ubeEEjHI1XX3g&oe=6570F18F", caption="Hello World", use_column_width=True)
+    
 # Function to render the about me page
 def about_me():
     st.title("About Me Page")
@@ -38,7 +28,7 @@ def contact():
     st.subheader("My social media accounts")
     st.write("You can contact me via Email at javegajelloma@gmail.com.")
     st.write("Or via facebook https://www.facebook.com/profile.php?id=100057120588260")
-    st.write("Fell free to message me")
+    st.write("Please click me https://boulderbugle.com/magnitude-61-na-lindol-5qyed30w")
 
 
 def main():
