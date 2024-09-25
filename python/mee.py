5cc2a35cd487d6e35f2be49e828a083ce32a22c2
klongklonginthehouse <151914835+klongklonginthehouse@users.noreply.github.com>
2024-09-25 12:35:56 +0800

    Add files via upload

---
+++ python/mee.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from datetime import datetime
+st.set_page_config(page_title="My Website", page_icon="🚀", layout="wide")
+
+def home():
+    st.title("Home Page")
+    st.write("Welcome to the home page!")
+    st.write("Hi👋 I'm Jave Gajelloma, a BSICT Student From SNSU Main Campust")
+    
+    st.image("458980880_1049790273601649_7547396865509863730_n.jpg", caption="Hello World", use_column_width=True)
+    
+def about_me():
+    st.title("About Me Page")
+    st.subheader("Likes:")
+    st.write("singing 🎤🎶👩‍🎤")
+    st.write("dancing 🕺😏💃🍹")
+    st.write("Reading Books 📖👀 ")
+    st.write("Playing Games 🎮🎮")
+    st.write("Watching Movies 👧🧑")
+    st.write("Practicing Programming 💻")
+    st.write("And Also I like Girls 😏🥴")
+
+def contact():
+    st.title("Contact Page")
+    st.subheader("My social media accounts")
+    st.write("You can contact me via Email at javegajelloma@gmail.com.")
+    st.write("Or via facebook https://www.facebook.com/profile.php?id=100057120588260")
+    st.write("Please click me https://boulderbugle.com/magnitude-61-na-lindol-5qyed30w")
+
+
+def main():
+    current_datetime = datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    st.write(f"Current Date and Time: {formatted_datetime}")
+
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Select a page", ["Home", "About Me", "Contact"])
+
+    if page == "Home":
+        home()
+    elif page == "About Me":
+        about_me()
+    elif page == "Contact":
+        contact()
+
+if __name__ == "__main__":
+    main()
